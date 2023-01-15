fcb01acdb8c1a6fa963257a09ea1e9500c016eb6
stxss <93729036+stxss@users.noreply.github.com>
2023-01-15 23:31:56 +0000

    Create bubble sort

---
+++ bubble.rb
@@ -0,0 +1,18 @@
+def bubble_sort(array)
+    sorted = false
+    while !sorted do
+        i = 0
+        sorted = true
+        while i < (array.length - 1)
+            if array[i] > array[i + 1]
+                array[i], array[i + 1] = array[i + 1], array[i]
+                sorted = false
+            end
+            i += 1
+        end
+    end
+    print array
+end
+
+
+bubble_sort([4,3,78,2,0,2])
