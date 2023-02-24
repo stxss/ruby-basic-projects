b931e6854145683ef5767e04b636b4498a632943
stxss <93729036+stxss@users.noreply.github.com>
2023-02-24 12:16:51 +0000

    Refactor bubble sort

--- bubble.rb
+++ bubble.rb
@@ -1,22 +1,23 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
-    sorted = false
-    until sorted do
-        i = 0
-        sorted = true
-        while i < (array.length - 1)
-            if array[i] > array[i + 1]
-                placeholder = array[i]
-                array[i] = array[i + 1]
-                array[i + 1] = placeholder
-                # or
-                # array[i], array[i + 1] = array[i + 1], array[i]
-                sorted = false
-            end
-            i += 1
-        end
+  sorted = false
+  until sorted
+    i = 0
+    sorted = true
+    while i < (array.length - 1)
+      if array[i] > array[i + 1]
+        placeholder = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = placeholder
+        # or
+        # array[i], array[i + 1] = array[i + 1], array[i]
+        sorted = false
+      end
+      i += 1
     end
-    p array
+  end
+  p array
 end
 
-
-bubble_sort([4,3,78,2,0,2])
+bubble_sort([4, 3, 78, 2, 0, 2])
