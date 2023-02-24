10c5a2bc7c8ae8c183a7cd536729e7e50fcc4b6d
stxss <93729036+stxss@users.noreply.github.com>
2023-02-24 13:05:19 +0000

    Refactor the caesar method into a class

--- caesar.rb
+++ caesar.rb
@@ -1,29 +1,37 @@
 # frozen_string_literal: true
 
-def caesar_cipher(string, shift_factor)
-  original_arr = []
+class Caesar
+  attr_accessor :string, :shift_factor
 
-  upper = ('A'..'Z').to_a
-  lower = ('a'..'z').to_a
+  def initialize
+    # print "Enter your string to encrypt: "
+    # string = gets.chomp
 
-  string.each_char { |c| original_arr.push(c) }
+    # print "Now enter your desired shift factor: "
+    # shift_factor = gets.chomp.to_i
 
-  new_arr = original_arr.map do |c|
-    if c.ord >= 65 && c.ord <= 90
-      upper[(c.ord - 65 + shift_factor) % upper.length]
-    elsif c.ord >= 97 && c.ord <= 122
-      lower[(c.ord - 97 + shift_factor) % lower.length]
-    else
-      c.ord.chr
-    end
+    @string = string
+    @shift_factor = shift_factor
+    # shift(string, shift_factor)
   end
-  p new_arr.join
+
+  def shift(string, shift_factor)
+    original_arr = []
+
+    upper = ("A".."Z").to_a
+    lower = ("a".."z").to_a
+
+    string.each_char { |c| original_arr.push(c) }
+
+    new_arr = original_arr.map do |c|
+      if c.ord >= 65 && c.ord <= 90
+        upper[(c.ord - 65 + shift_factor) % upper.length]
+      elsif c.ord >= 97 && c.ord <= 122
+        lower[(c.ord - 97 + shift_factor) % lower.length]
+      else
+        c.ord.chr
+      end
+    end
+    p new_arr.join
+  end
 end
-
-print 'Enter your string to encrypt: '
-string = gets.chomp
-
-print 'Now enter your desired shift factor: '
-shift_factor = gets.chomp.to_i
-
-caesar_cipher(string, shift_factor)
