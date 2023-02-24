c15bf1c080b582d8c54a789216bebba79ecc4b4c
stxss <93729036+stxss@users.noreply.github.com>
2023-02-24 12:17:54 +0000

    Refactor substrings

--- substrings.rb
+++ substrings.rb
@@ -1,17 +1,16 @@
+# frozen_string_literal: true
+
 def substrings(string, dictionary)
-    arr = string.gsub(/[^a-zA-Z]/, " ").downcase.split()
-    result = Hash.new(0)
-    dictionary.map do |word|
-        arr.each do |chars| 
-            if chars.include?(word)
-                result[word] += 1
-            end
-        end
+  arr = string.gsub(/[^a-zA-Z]/, ' ').downcase.split
+  result = Hash.new(0)
+  dictionary.map do |word|
+    arr.each do |chars|
+      result[word] += 1 if chars.include?(word)
     end
-    p result
+  end
+  p result
 end
 
-
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+substrings('below', dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
